4461ce14b7344496c34b982874549163e1cdb0ad
zerontec <zerontecnologia1@gmail.com>
2023-03-16 21:11:23 -0400

    build initial development environment of the backend

---
+++ api/moduleAuthen/authen-microservices/src/main/java/com/authentication/authenmicroservices/AuthenMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.authentication.authenmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenMicroservicesApplication.class, args);
+	}
+
+}
---
+++ api/moduleAuthen/authen-microservices/src/test/java/com/authentication/authenmicroservices/AuthenMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authentication.authenmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
